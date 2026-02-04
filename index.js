208d2edd1708ae580c952eb3e00813c8c0094bef
Chumus <vladiktimofeev50@gmail.com>
2026-02-04 17:29:07 +0200

    Added Header

---
+++ index.js
@@ -0,0 +1,6 @@
+const Dropdown_Button = document.querySelector(".Dropdown_Button");
+const Dropdown_Menu = document.querySelector(".Dropdown_Menu");
+
+Dropdown_Button.addEventListener("click", () => {
+    Dropdown_Menu.classList.toggle("Active");
+});
