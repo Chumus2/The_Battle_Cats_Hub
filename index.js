7807794446a56deafb64bf929e679eeade4f4dc2
Chumus <vladiktimofeev50@gmail.com>
2026-02-05 12:39:57 +0200

    added adaptive language button for phones

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const Languages_Menu = document.querySelector(".Languages_Menu")
 
 
 Dropdown_Button.addEventListener("click", () => {
+
+    Languages_Menu.classList.remove("Active");
+    Languages_Button.classList.remove("Active");
+
     Dropdown_Menu.classList.toggle("Active");
     Dropdown_Button.classList.toggle("Active");
+
 });
 
 Languages_Button.addEventListener("click", () => {
+
+    Dropdown_Menu.classList.remove("Active");
+    Dropdown_Button.classList.remove("Active");
+
     Languages_Menu.classList.toggle("Active");
     Languages_Button.classList.toggle("Active");
+    
 });
