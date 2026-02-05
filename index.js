d0d742eed4d7c1c980aa80a0c820ce3143f0e493
Chumus <vladiktimofeev50@gmail.com>
2026-02-05 12:10:37 +0200

    added languages button

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const Dropdown_Button = document.querySelector(".Dropdown_Button");
 const Dropdown_Menu = document.querySelector(".Dropdown_Menu");
 
+const Languages_Button = document.querySelector(".Languages_Button")
+const Languages_Menu = document.querySelector(".Languages_Menu")
+
+
 Dropdown_Button.addEventListener("click", () => {
     Dropdown_Menu.classList.toggle("Active");
     Dropdown_Button.classList.toggle("Active");
+});
+
+Languages_Button.addEventListener("click", () => {
+    Languages_Menu.classList.toggle("Active");
+    Languages_Button.classList.toggle("Active");
 });
